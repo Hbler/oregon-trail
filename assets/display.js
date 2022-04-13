b9f7ef90d8e40596e3a423e65ef74b28f2cf57bd
Hugo Ernesto <mail.hbler@gmail.com>
2022-04-13 12:53:34 -0300

    update: added info

--- assets/display.js
+++ assets/display.js
@@ -7,11 +7,13 @@ const Main = document.createElement("main");
 function prepareLayout() {
   const title = document.createElement("h1");
   const subT = document.createElement("h2");
+  const info = document.createElement("p");
 
   title.innerText = "S3-13 POO: Oregon Trail Pt.02";
   subT.innerText = "M2 - Jan/22 - Hugo Bler";
+  info.innerText = "Tests on the console.";
 
-  Header.append(title, subT);
+  Header.append(title, subT, info);
   //   Main.append();
   body.append(Header, Main);
 }
